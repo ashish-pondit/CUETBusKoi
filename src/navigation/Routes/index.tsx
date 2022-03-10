d1e169ed4dd3f0a2e596d4c26720796a764f18f8
Ashish Pondit <ashishpondit@gmail.com>
2022-03-11 03:27:29 +0600

    tab navigation barely complete

--- src/navigation/Routes/index.tsx
+++ src/navigation/Routes/index.tsx
@@ -2,8 +2,14 @@ import * as React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-function HomeScreen() {
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+const Tab = createBottomTabNavigator();
+
+function HomeScreen({navigation}) {
+  // console.log(navigation);
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Test Home Screen</Text>
@@ -24,10 +30,36 @@ const Stack = createNativeStackNavigator();
 function Routes() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Output" component={OutputPage} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName: string = 'Home';
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'ios-bus' : 'ios-bus-outline';
+            } else if (route.name === 'Output') {
+              iconName = focused ? 'ios-location' : 'ios-location-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}>
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          // options={{
+          //   tabBarIcon: ({focused}) => {
+          //     // You can return any component that you like here
+          //     return <Ionicons name="ios-list" size={25} />;
+          //   },
+          // }}
+        />
+        <Tab.Screen name="Output" component={OutputPage} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
