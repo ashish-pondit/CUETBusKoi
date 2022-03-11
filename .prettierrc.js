306297e39ee2c20e305c78310ae3ca0c13a84290
rukon023 <rukon219@gmail.com>
2022-03-11 11:52:44 +0600

    prettier updated

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,14 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
   arrowParens: 'avoid',
+  printWidth: 80,
+  'editor.formatOnSave': true,
+  proseWrap: 'always',
+  tabWidth: 4,
+  requireConfig: false,
+  useTabs: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  semi: true,
 };
