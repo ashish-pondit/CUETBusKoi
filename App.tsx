4f3e294e2b5381386a59c6c7c64d6cc87a34d74a
Ashish Pondit <ashishpondit@gmail.com>
2022-03-11 14:50:15 +0600

    deleted buslist.js

--- App.tsx
+++ App.tsx
@@ -2,39 +2,39 @@ import React from 'react';
 import BusButton from './src/component/BusButton';
 import BusList from './src/screens/BusList';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
 } from 'react-native';
 
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
+    Colors,
+    DebugInstructions,
+    Header,
+    LearnMoreLinks,
+    ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
 import Routes from './src/navigation/Routes';
 
 const App = () => {
-  return (
-    <View style={styles.continer}>
-      {/* <BusList/> */}
-      
-      <Routes />
-    </View>
-  );
+    return (
+        <View style={styles.continer}>
+            {/* <BusList/> */}
+
+            <Routes />
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  continer: {
-    flex: 1,
-  },
+    continer: {
+        flex: 1,
+    },
 });
 
 export default App;
