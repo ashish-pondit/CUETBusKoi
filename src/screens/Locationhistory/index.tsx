d03b3eb04ee496b4cf26249aa47d14721c0116ba
Md. Nahidul Islam Opu <37839072+nahidul-opu@users.noreply.github.com>
2022-03-12 12:51:45 +0600

    location data from firebase

--- src/screens/Locationhistory/index.tsx
+++ src/screens/Locationhistory/index.tsx
@@ -3,9 +3,48 @@ import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import BusLocationCard from '../../component/BusLocationCard';
 import { busData } from '../../data/busList.json';
 import { colorList } from '../../config';
-import Buslocationlist from '../../component/Buslocationlist';
-
+//import Buslocationlist from '../../component/Buslocationlist';
+import Firebase from '../../config';
+import { getDatabase, onValue, get, ref, child } from 'firebase/database';
+import { getType } from 'react-native-device-info';
 function Locationhistory() {
+    const [allBusData, setAllBusData] = React.useState<{ [key: string]: any }>(
+        {},
+    );
+    const reference = ref(getDatabase(Firebase), 'buses/');
+    var loaded: boolean = false;
+    var i = 0;
+    function setFirebaseData() {
+        get(child(ref(getDatabase(Firebase)), 'buses/')).then(snapshot => {
+            var arr: { [key: string]: any } = {};
+            var data = snapshot.val();
+            for (var i in busData) {
+                var bName = busData[i].busName;
+                var latlong = [];
+                if (bName in data) {
+                    for (var device in data[bName]) {
+                        latlong.push(data[bName][device]);
+                    }
+                    arr[bName] = latlong[0];
+                } else {
+                    arr[bName] = [];
+                }
+            }
+            setAllBusData(arr);
+            //console.log(allBusData['মাতামুহুরি']);
+            //console.log(arr);
+        });
+    }
+    React.useEffect(() => {
+        if (!loaded) {
+            loaded = true;
+            setFirebaseData();
+        }
+        const interval = setInterval(() => {
+            setFirebaseData();
+        }, 30000);
+        return () => clearInterval(interval);
+    }, []);
     function busList(data: any) {
         let buslocationList = [];
         for (let i = 0; i < data.length; i++) {
