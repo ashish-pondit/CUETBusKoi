7cc1372ac2202e64cbf6e844b8c6bacdd4ab870a
rukon023 <rukon219@gmail.com>
2022-03-17 13:03:14 +0600

    collapsible component added

--- src/screens/About/index.tsx
+++ src/screens/About/index.tsx
@@ -7,25 +7,67 @@ import {
     StyleSheet
 } from 'react-native';
 import { fontConfig, colorList, spacing } from "../../config/index"
+import CollapsibleView from "@eliav2/react-native-collapsible-view";
 
 const About = () => {
     return (
         <SafeAreaView>
-            <Text>
-                TTTTTTTTEEEEEEEESSSSSSTTTTT
-            </Text>
+            <CollapsibleView
+                style={styles.container}
+                arrowStyling={styles.arrowStyle}
+                touchableWrapperStyle={styles.wrapperStyle}
+                // titleStyle={styles.titleStyle}
+                title={<Text style={styles.titleStyle}>How to Use</Text>}
+                collapsibleContainerStyle={{}}
+            >
+                <Text>hey there! we believe that you can understand and use this simple app.</Text>
+            </CollapsibleView>
+
+
+            <CollapsibleView
+                arrowStyling={styles.arrowStyle}
+                title={<Text style={styles.titleStyle}>Developer Info</Text>}
+            >
+                <Text>.........</Text>
+            </CollapsibleView>
+
+            <CollapsibleView
+                arrowStyling={styles.arrowStyle}
+                title={<Text style={styles.titleStyle}>Contact</Text>}
+            >
+                <Text>u1604071@student.cuet.ac.bd</Text>
+                <Text>u1604073@student.cuet.ac.bd</Text>
+                <Text>u1604023@student.cuet.ac.bd</Text>
+            </CollapsibleView>
         </SafeAreaView>
+
     );
 };
 
 export default About;
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        paddingLeft: 50,
+        textAlign: 'left',
+        alignItems: 'flex-start',
+        backgroundColor: colorList.secondary
     },
     textStyle: {
         fontSize: fontConfig.xxlg,
         alignSelf: 'center'
+    },
+    arrowStyle: {
+        color: colorList.primary,
+        thickness: 2,
+    },
+    titleStyle: {
+        color: 'blue',
+        fontSize: fontConfig.md,
+        fontWeight: 'bold',
+    },
+    wrapperStyle: {
+        //backgroundColor: 'black',
+        paddingLeft: 10
     }
 });
