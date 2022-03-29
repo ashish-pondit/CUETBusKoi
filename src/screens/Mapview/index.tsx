0211d8c6fb7e6f58025c270afc888908991968ac
Ashish Pondit <ashishpondit@gmail.com>
2022-03-30 01:56:01 +0600

    marker list added

--- src/screens/Mapview/index.tsx
+++ src/screens/Mapview/index.tsx
@@ -8,9 +8,20 @@ import {
     useColorScheme,
     View,
 } from 'react-native';
-import { colorList } from '../../config';
+import { colorList, spacing } from '../../config';
 
 import RNMapView, { Circle, Marker } from 'react-native-maps';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const Warningmessage = () => {
+    return (
+        <View>
+            <Text>
+                Sorry, this bus location is unknown. Please try again later.
+            </Text>
+        </View>
+    );
+};
 
 const Mapview = ({ route }) => {
     console.log('printing inside map view');
@@ -25,93 +36,128 @@ const Mapview = ({ route }) => {
 
     const markerList = (alldata, target) => {
         var markerArray = [];
-
-        for (let i = 0; i < alldata.length; i++) {
-            console.log('whay this colabary');
+        var targetBusdata = alldata[0];
+        let i = 0;
+        for (i = 0; i < alldata.length; i++) {
+            console.log('why this colabary');
             console.log(alldata[i]);
-            // console.log(alldata[i].location[i].latitude);
-            // console.log(alldata[i].location[i].longitude);
-            var lt = alldata[i].location[0].latitude;
-            var lg = alldata[i].location[0].longitude;
-            // if (lt + lg !== 0) {
-            // }
-            if (alldata[i].busName === target) {
-                markerArray.push(
-                    <Marker
-                        anchor={{ x: 0.5, y: 0.6 }}
-                        coordinate={{
-                            latitude: alldata[i].location[0].latitude,
-                            longitude: alldata[i].location[0].longitude,
-                        }}
-                        key={i}
-                    >
-                        <View style={styles.dotGreen}>
-                            {/* <Text>padma</Text> */}
-                        </View>
-                    </Marker>,
-                );
-            } else {
-                markerArray.push(
-                    <Marker
-                        anchor={{ x: 0.5, y: 0.6 }}
-                        coordinate={{
-                            latitude: alldata[i].location[0].latitude,
-                            longitude: alldata[i].location[0].longitude,
-                        }}
-                        key={i}
-                    >
-                        <View style={styles.dotBlue}>
-                            {/* <Text>padma</Text> */}
-                        </View>
-                    </Marker>,
-                );
+
+            // console.log('printing console logic ');
+            // console.log(alldata[i].busName);
+            // console.log(alldata[i].busName === target);
+
+            if (
+                alldata[i].location[0].latitude !== 0 &&
+                alldata[i].location[0].longitude !== 0
+            ) {
+                if (alldata[i].busName === target) {
+                    targetBusdata = alldata[i];
+                    // markerArray.push(
+                    //     <Marker
+                    //         anchor={{ x: 0.5, y: 0.65 }}
+                    //         coordinate={{
+                    //             latitude: alldata[i].location[0].latitude,
+                    //             longitude: alldata[i].location[0].longitude,
+                    //         }}
+                    //         key={i}
+                    //     >
+                    //         <View style={styles.markerContainer}>
+                    //             <View style={styles.markerNameView}>
+                    //                 <Text style={styles.markerName}>
+                    //                     {alldata[i].busName}
+                    //                 </Text>
+                    //             </View>
+                    //             <View style={styles.markerJoin} />
+                    //             <View style={styles.dotGreen}>
+                    //                 {/* <Text>padma</Text> */}
+                    //             </View>
+                    //         </View>
+                    //     </Marker>,
+                    // );
+                } else {
+                    markerArray.push(
+                        <Marker
+                            anchor={{ x: 0.5, y: 0.65 }}
+                            coordinate={{
+                                latitude: alldata[i].location[0].latitude,
+                                longitude: alldata[i].location[0].longitude,
+                            }}
+                            key={i}
+                        >
+                            <View style={styles.markerContainer}>
+                                <View style={styles.markerNameView}>
+                                    <Text style={styles.markerName}>
+                                        {alldata[i].busName}
+                                    </Text>
+                                </View>
+                                <View style={styles.markerJoin} />
+                                <View style={styles.dotBlue}>
+                                    {/* <Text>padma</Text> */}
+                                </View>
+                            </View>
+                        </Marker>,
+                    );
+                }
             }
         }
+
+        console.log('printing markerlist array output');
+        console.log(markerArray);
+        console.log('checking again bus name');
+        console.log(targetBusdata);
+
+        markerArray.push(
+            <Marker
+                anchor={{ x: 0.5, y: 0.65 }}
+                coordinate={{
+                    latitude: targetBusdata.location[0].latitude,
+                    longitude: targetBusdata.location[0].longitude,
+                }}
+                key={i}
+            >
+                <View style={[styles.markerContainer]}>
+                    <View style={styles.markerNameView}>
+                        <Text style={styles.markerName}>
+                            {targetBusdata.busName}
+                        </Text>
+                    </View>
+                    <View style={styles.markerJoin} />
+                    <View style={styles.dotGreen}>
+                        {/* <Text>padma</Text> */}
+                    </View>
+                </View>
+            </Marker>,
+        );
 
         return markerArray;
     };
 
     return (
         <View style={{ flex: 1 }}>
-            <RNMapView
-                ref={mapRef}
-                initialCamera={{
-                    altitude: 1500,
-                    center: {
-                        latitude: 22.4616622,
-                        longitude: 91.969816,
-                    },
-                    heading: 0,
-                    pitch: 0,
-                    zoom: 11,
-                }}
-                loadingEnabled
-                loadingBackgroundColor={colorList.secondary}
-                style={StyleSheet.absoluteFillObject}
-                rotateEnabled={false}
-            >
-                {/* {markerList(data, curBusname)} */}
-                <Marker
-                    anchor={{ x: 0.5, y: 0.6 }}
-                    coordinate={{
-                        latitude: 22.4616622,
-                        longitude: 91.969816,
+            {(currentBus.location[0].latitude === 0,
+            currentBus.location[0].longitude === 0) ? (
+                <Warningmessage />
+            ) : (
+                <RNMapView
+                    ref={mapRef}
+                    initialCamera={{
+                        altitude: 500,
+                        center: {
+                            latitude: currentBus.location[0].latitude,
+                            longitude: currentBus.location[0].longitude,
+                        },
+                        heading: 0,
+                        pitch: 0,
+                        zoom: 11,
                     }}
+                    loadingEnabled
+                    loadingBackgroundColor={colorList.secondary}
+                    style={StyleSheet.absoluteFillObject}
+                    rotateEnabled={false}
                 >
-                    <View style={styles.dotBlue}>
-                        {/* <Text>padma</Text> */}
-                    </View>
-                </Marker>
-                {/* <Marker
-                    anchor={{ x: 0.5, y: 0.6 }}
-                    coordinate={{
-                        latitude: 22.4616622,
-                        longitude: 91.969816,
-                    }}
-                >
-                    <View style={styles.dotGreen}></View>
-                </Marker> */}
-            </RNMapView>
+                    {markerList(data, curBusname)}
+                </RNMapView>
+            )}
         </View>
     );
 };
@@ -144,9 +190,9 @@ const styles = StyleSheet.create({
         backgroundColor: 'rgb(50, 180, 50)',
         width: 24,
         height: 24,
-        borderWidth: 3,
-        borderColor: 'white',
-        borderRadius: 12,
+        borderWidth: 2,
+        borderColor: colorList.primary,
+        borderRadius: 20,
         shadowColor: 'black',
         shadowOffset: {
             width: 1,
@@ -156,6 +202,7 @@ const styles = StyleSheet.create({
         shadowRadius: 1.5,
         elevation: 4,
     },
+
     arrow: {
         width: 0,
         height: 0,
@@ -167,6 +214,28 @@ const styles = StyleSheet.create({
         borderLeftColor: 'transparent',
         borderRightColor: 'transparent',
         borderBottomColor: 'rgb(0, 120, 255)',
+    },
+    markerContainer: {
+        alignItems: 'center',
+    },
+    markerNameView: {
+        backgroundColor: colorList.primary,
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: 'white',
+        shadowOpacity: 0.3,
+        shadowRadius: 1.5,
+        elevation: 4,
+    },
+    markerName: {
+        fontSize: 20,
+        color: 'white',
+        margin: spacing.sm,
+    },
+    markerJoin: {
+        height: 10,
+        width: 5,
+        backgroundColor: colorList.primary,
     },
 });
 
