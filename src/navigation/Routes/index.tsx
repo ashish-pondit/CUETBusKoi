00ad3c2a449fd57aacc8a2ed6cff98457519a22f
Ashish Pondit <ashishpondit@gmail.com>
2022-05-21 02:16:10 +0600

    Scheduled added
    schedule working hour added but need minor fixes

--- src/navigation/Routes/index.tsx
+++ src/navigation/Routes/index.tsx
@@ -9,25 +9,112 @@ import BusList from '../../screens/BusInputGrid';
 const Tab = createBottomTabNavigator();
 import About from '../../screens/About';
 import MapView from '../../screens/Locationhistory/MapView';
+import { daydata } from '../../data/weekday.json';
+import Firebase from '../../config';
+import { getDatabase, get, ref, child } from 'firebase/database';
+import Wrongpage from '../../screens/Wrongpage';
 
 import HomeStack from '../HomeStack';
 
-//dummy screens
-
-// const About = () => {
-//     return (
-//         <MapView
-//             coords={{
-//                 longitude: 91.969816,
-//                 latitude: 22.4616622,
-//                 accuracy: 90,
-//                 heading: -1,
-//             }}
-//         />
-//     );
-// };
-
 function Routes() {
+    const [schedule, setschedule] = React.useState({
+        ack: 0,
+        nextSchedule: { start: 0, end: 0 },
+    });
+
+    // const [weekday, setweekday] = React.useState(null);
+    // const [saturday, setsaturday] = React.useState(null);
+
+    var daynum = 1; //new Date().getDay();
+    var dayhour = 12; //new Date().getHours();
+
+    var saturday = null;
+    var weekday = null;
+    //function to check bus schedule
+    function check_schedule() {
+        // console.log('printing info');
+        // console.log('daynum : ', daynum);
+        // console.log('dayhour :', dayhour);
+        if (daynum in [1, 2, 3, 4, 7]) {
+            // console.log('checking type of weekday');
+            // console.log(typeof weekday);
+            // console.log(weekday);
+            for (let i = 0; i < weekday.length; i++) {
+                console.log('checking ');
+                console.log(dayhour, weekday[i]['start'], weekday[i]['end']);
+                if (
+                    dayhour >= weekday[i]['start'] &&
+                    dayhour <= weekday[i]['end']
+                ) {
+                    setschedule({
+                        ack: 1,
+                        nextSchedule: {
+                            start: weekday[i]['start'],
+                            end: weekday[i]['end'],
+                        },
+                    });
+                    break;
+                } else if (dayhour < weekday[i]['start']) {
+                    setschedule({
+                        ack: 0,
+                        nextSchedule: {
+                            start: weekday[i]['start'],
+                            end: weekday[i]['end'],
+                        },
+                    });
+                    break;
+                }
+            }
+        } else if (daynum === 6) {
+            for (let i = 0; i < saturday.length; i++) {
+                console.log('checking ');
+                console.log(dayhour, saturday[i]['start'], saturday[i]['end']);
+                if (
+                    dayhour >= saturday[i]['start'] &&
+                    dayhour <= saturday[i]['end']
+                ) {
+                    setschedule({
+                        ack: 1,
+                        nextSchedule: {
+                            start: saturday[i]['start'],
+                            end: saturday[i]['end'],
+                        },
+                    });
+                } else if (dayhour < saturday[i]['start']) {
+                    setschedule({
+                        ack: 0,
+                        nextSchedule: {
+                            start: saturday[i]['start'],
+                            end: saturday[i]['end'],
+                        },
+                    });
+                }
+            }
+        }
+    }
+
+    React.useEffect(() => {
+        get(child(ref(getDatabase(Firebase)), 'schedule/')).then(snapshot => {
+            var data = snapshot.val();
+
+            console.log('getting data from firebase');
+            console.log(data);
+            // console.log(JSON.parse(data));
+            console.log(data['saturday']);
+            // console.log(JSON.parse(data['saturday']));
+            saturday = JSON.parse(data['saturday']);
+            weekday = JSON.parse(data['weekday']);
+
+            check_schedule();
+        });
+        console.log('checking schedule again');
+        console.log(schedule);
+    }, []);
+
+    console.log('checking schedule ');
+    console.log(schedule);
+    // console.log('week day is: ', daydata);
+
     return (
         <NavigationContainer>
             <Tab.Navigator
@@ -46,6 +133,10 @@ function Routes() {
                             iconName = focused
                                 ? 'alert-circle'
                                 : 'alert-circle-outline';
+                        } else if (route.name === 'Wrong') {
+                            iconName = focused
+                                ? 'calendar'
+                                : 'calendar-outline';
                         }
 
                         return (
@@ -78,16 +169,30 @@ function Routes() {
                     // },
                 })}
             >
-                <Tab.Screen
-                    name="Homescreen"
-                    component={HomeStack}
-                    options={{ title: 'Bus Location', headerShown: false }}
-                />
-                <Tab.Screen
-                    name="Input"
-                    component={BusList}
-                    options={{ title: 'Update Location' }}
-                />
+                {schedule['ack'] === 1 ? (
+                    <>
+                        <Tab.Screen
+                            name="Homescreen"
+                            component={HomeStack}
+                            options={{
+                                title: 'Bus Location',
+                                headerShown: false,
+                            }}
+                        />
+                        <Tab.Screen
+                            name="Input"
+                            component={BusList}
+                            options={{ title: 'Update Location' }}
+                        />
+                    </>
+                ) : (
+                    <Tab.Screen
+                        name="Wrong"
+                        component={Wrongpage}
+                        options={{ title: 'unscheduled time' }}
+                        initialParams={schedule}
+                    />
+                )}
 
                 <Tab.Screen
                     name="About"
---
+++ src/screens/Wrongpage/index.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    Button,
+    TextInput,
+    SafeAreaView,
+    StyleSheet,
+    Alert,
+    ToastAndroid,
+    SectionList,
+    ScrollView,
+} from 'react-native';
+import Firebase, { fontConfig, colorList, spacing } from '../../config/index';
+import CollapsibleView from '@eliav2/react-native-collapsible-view';
+import { getDatabase, ref, set } from 'firebase/database';
+import { getUniqueId } from 'react-native-device-info';
+import BusButton from '../../component/BusButton';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+const Wrongpage = ({ route, navigation }) => {
+    const { nextSchedule } = route.params;
+    console.log('printing received parameter');
+    console.log(nextSchedule);
+    return (
+        <View style={styles.body}>
+            <FontAwesome5
+                name="sad-tear"
+                size={200}
+                color={colorList.primary}
+            />
+            <Text style={styles.textDesign}>
+                Sorry, there is no bus scheduled at the moment.
+            </Text>
+            {nextSchedule['start'] ? (
+                <Text style={styles.textSecondary}>
+                    Next bus is scheduled at: {nextSchedule['start'] % 12}{' '}
+                    {nextSchedule['start'] > 12 ? 'PM' : 'AM'}
+                </Text>
+            ) : null}
+
+            {/* (
+                <Text style={styles.textSecondary}>
+                    No more bus scheduled for today.
+                </Text>
+            ) */}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    body: {
+        flex: 1,
+        backgroundColor: colorList.secondary,
+        padding: spacing.lg,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textDesign: {
+        fontSize: 20,
+        color: colorList.darkSoft,
+        marginTop: spacing.lg,
+    },
+    textSecondary: {
+        fontSize: 18,
+        color: colorList.warning,
+    },
+});
+
+export default Wrongpage;
